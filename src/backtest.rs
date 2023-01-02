849e5d9d9d494d4779432e986050ae6842b5611c
GitNiko <galaxis.ling@gmail.com>
2023-01-02 13:53:25 +0800

    move MACross strategy implementation to unit test

--- src/backtest.rs
+++ src/backtest.rs
@@ -1,6 +1,4 @@
-use chrono::{Duration, TimeZone, Utc};
-use rust_decimal::prelude::Signed;
-use ta::{indicators::ExponentialMovingAverage, Next};
+
 
 use crate::{
     data::{DayTradeUnit, DayTradeUnitIter, StockCode, StockTradeData, TradeDataSource},
@@ -33,36 +31,10 @@ pub trait Strategy {
     fn next(&mut self, account: &mut Account, stock_trade_info: &Vec<DayTradeUnit>);
 }
 
-pub struct MACross {
-    ema5: ExponentialMovingAverage,
-    ema10: ExponentialMovingAverage,
-    EMA5: Vec<f64>,
-    EMA10: Vec<f64>
-}
-
-impl MACross {
-    fn new() -> Self {
-        let ema5 = ExponentialMovingAverage::new(5).unwrap();
-        let ema10 = ExponentialMovingAverage::new(10).unwrap();
-        MACross { ema5, ema10, EMA5: Vec::new(), EMA10: Vec::new() }
-    }
-}
-
-impl Strategy for MACross {
-    fn next(&mut self, account: &mut Account, stock_trade_info: &Vec<DayTradeUnit>) {
-        if let Some(today) = stock_trade_info.last() {
-            let ema5 = self.ema5.next(f64::from(today.trade_data.close));
-            let ema10 = self.ema10.next(f64::from(today.trade_data.close));
-            self.EMA5.push(ema5);
-            self.EMA10.push(ema10);
-            println!("{:?}", ema5);
-        }
-    }
-}
-
 
 impl<'a> BackTest<'a> {
     fn new(code: StockCode, strategy: &'a mut dyn Strategy) -> Result<Self> {
+        // todo: change to builder pattern
         let mut account = Account::new("100000", "0.00025")?;
         Ok(BackTest {
             code,
@@ -93,7 +65,38 @@ impl<'a> BackTest<'a> {
 
 #[cfg(test)]
 mod tests {
-    use super::{BackTest, MACross, Strategy};
+    use ta::{indicators::ExponentialMovingAverage, Next};
+
+    use crate::{strategy::Account, data::DayTradeUnit};
+
+    use super::{BackTest, Strategy};
+
+    pub struct MACross {
+        ema5: ExponentialMovingAverage,
+        ema10: ExponentialMovingAverage,
+        EMA5: Vec<f64>,
+        EMA10: Vec<f64>
+    }
+
+    impl MACross {
+        fn new() -> Self {
+            let ema5 = ExponentialMovingAverage::new(5).unwrap();
+            let ema10 = ExponentialMovingAverage::new(10).unwrap();
+            MACross { ema5, ema10, EMA5: Vec::new(), EMA10: Vec::new() }
+        }
+    }
+
+    impl Strategy for MACross {
+        fn next(&mut self, account: &mut Account, stock_trade_info: &Vec<DayTradeUnit>) {
+            if let Some(today) = stock_trade_info.last() {
+                let ema5 = self.ema5.next(f64::from(today.trade_data.close));
+                let ema10 = self.ema10.next(f64::from(today.trade_data.close));
+                self.EMA5.push(ema5);
+                self.EMA10.push(ema10);
+                println!("{:?}", ema5);
+            }
+        }
+    }
 
     #[test]
     fn iter_day_info() {
